db054b552678c98e67f184b490f8299eb185fef6
Amanda Thorne <manda.thorne@gmail.com>
2016-11-21 16:05:39 -0600

    working on adding an item

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,13 @@
 app.controller('MainController', function ($scope) {
-
+  $scope.todos = [
+    'Learn Angular',
+    'Commit once a day',
+    'Get a JavaScript tattoo'
+  ];
+  $scope.data = {};
+  $scope.add = function() {
+    console.log($scope.data.task.$modelValue);
+    // $scope.todos.push($scope.newToDo);
+    // console.log('i\'m trying');
+  }
 });
